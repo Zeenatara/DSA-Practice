7e0fb5efe437e19abea132bff49f85aa99c4c26d
Zeenat Ara <arazeenat747@gmail.com>
2026-04-04 16:57:52 +0530

    check Armstrong

--- src/JavaBasics/checkArmstrong.java
+++ src/JavaBasics/checkArmstrong.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 public class checkArmstrong {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number : ");
-        int n=sc.nextInt();
-        String str=Integer.toString(n);
-int digit=String.valueOf(n).length();
-        int sum=0;
-        int temp=n;
-       while(n>0){
-           int digits =n%10;
-           sum+=Math.pow(digits,digit);
-           n/=10;
-       }
-      if(sum==temp){
-          System.out.println(temp + " The number is an Armstrong number");
-      }else{
-          System.out.println(temp + " The number is NOT an Armstrong number");
-      }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        int temp = n;
+        int sum = 0;
+        int digit = String.valueOf(n).length();
+
+        while (n > 0) {
+            int digits = n % 10;
+            sum += (int) Math.pow(digits, digit); // cast to int
+            n /= 10;
+        }
+
+        if (sum == temp) {
+            System.out.println(temp + " is an Armstrong number");
+        } else {
+            System.out.println(temp + " is NOT an Armstrong number");
+        }
+
+        sc.close();
     }
 }

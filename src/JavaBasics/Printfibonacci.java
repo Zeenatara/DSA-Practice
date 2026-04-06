585bc88adcb9b763c7b6d96314eb1fadbb75c222
Zeenat Ara <arazeenat747@gmail.com>
2026-04-06 12:14:34 +0530

    initial commit

---
+++ src/JavaBasics/Printfibonacci.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Printfibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+
+        int a=0,b=1;
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+
+            int next=a+b;
+            a=b;
+            b=next;
+        }
+
+    }
+}

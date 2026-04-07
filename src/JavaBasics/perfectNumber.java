a4af79c3fca00f135f9273cbd783c07cd21c53bc
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-04-07 06:47:16 +0530

    Implement perfect number

---
+++ src/JavaBasics/perfectNumber.java
@@ -0,0 +1,24 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class perfectNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int sum=0;
+
+        for (int i=1;i<=n/2;i++){
+            if(n % i==0){
+                sum+=i;
+
+            }
+        }
+        if(sum==n)
+            System.out.println("PERFECT Number");
+        else
+            System.out.println("NOT a PERFECT Number");
+
+    }
+}

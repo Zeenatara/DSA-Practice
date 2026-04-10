8d1bd8a192bec4e639b6e81f1270de32093e3dd7
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-04-10 23:56:45 +0530

    check Leap year

---
+++ src/JavaBasics/leapYear.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class leapYear {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int year=sc.nextInt();
+        if(year%400==0 || (year%4==0 && year%100!=0)){
+            System.out.println("leap");
+
+        }else{
+            System.out.println("not a leap year");
+        }
+    }
+}

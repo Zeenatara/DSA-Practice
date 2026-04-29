725f8f48e05daa05d86f88996c87449f57500abf
Zeenat Ara <aec.ece.zeenatara.2027.082@gmail.com>
2026-04-29 19:21:20 +0530

    Add maximumSubArray Sum

---
+++ src/ProblemsOnArray/Medium/kadaneAlgorithm.java
@@ -0,0 +1,44 @@
+package ProblemsOnArray.Medium;
+
+import java.util.Scanner;
+
+public class kadaneAlgorithm {
+
+    public static int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = maxSubArray(nums);
+
+        System.out.println("Maximum Subarray Sum: " + result);
+
+        sc.close();
+    }
+}
